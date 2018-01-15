28fa7e2080592e1d7daa7791493561efcc089464
Alperen Bozkurt <bozkurttalperen@gmail.com>
2018-01-15 23:56:53 +0300

    Responsive tasarıma geçildi

--- sehir.html.php
+++ sehir.html.php
@@ -1,7 +1,7 @@
 <div class="city shadow">
   <?php
     if ($clock == -1) {
-      echo '<h2 class="text-'.$color.'">'.$data['name'].' '.round($data['main']['temp']).'°</h2>';
+      echo '<h2 class="text-'.$color.'">'.$data['name'].'<br>'.round($data['main']['temp']).'°</h2>';
       echo '<h3>'.humanize($data['weather'][0]['description']).'</h3><br>';
       echo '<div class="img"><img src="img/'.$data['weather'][0]['icon'].'.png"></div><br>';
     }
