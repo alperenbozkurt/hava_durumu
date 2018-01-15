2d5da23d8d9d3891241c78b5522fc0a8968aa92e
Alperen Bozkurt <bozkurttalperen@gmail.com>
2018-01-15 05:03:51 +0300

    Ufak iyileştirmeler(dereceler yucarlandı, il divleri ortalandı)

--- sehir.html.php
+++ sehir.html.php
@@ -1,6 +1,6 @@
 <div class="city shadow">
   <?php
-    echo '<h2 style="text-align: center;">'.$data['name'].' '.$data['main']['temp'].'°</h2>';
+    echo '<h2 style="text-align: center;">'.$data['name'].' '.round($data['main']['temp']).'°</h2>';
     echo '<h3>'.humanize($data['weather'][0]['description']).'</h3><br>';
     echo '<div class="img"><img src="img/'.$data['weather'][0]['icon'].'.png"></div><br>';
   ?>
