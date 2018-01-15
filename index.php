e5abf2273e8abc37300315784e7462b594361559
Alperen Bozkurt <bozkurttalperen@gmail.com>
2018-01-15 21:56:47 +0300

    Tüm Şehirler eklendi.

--- index.php
+++ index.php
@@ -29,14 +29,87 @@
             <i class="dropdown icon"></i>
             <div class="default text">Şehirler</div>
             <div class="menu">
-              <div class="item" data-value="ankara">Ankara</div>
-              <div class="item" data-value="istanbul">İstanbul</div>
-              <div class="item" data-value="izmir">İzmir</div>
-              <div class="item" data-value="antalya">Antalya</div>
-              <div class="item" data-value="kastamonu">Kastamonu</div>
-              <div class="item" data-value="bursa">Bursa</div>
-              <div class="item" data-value="erzincan">Erzincan</div>
-              <div class="item" data-value="kırşehir">Kırşehir</div>
+              <div class="item" data-value="Adana">Adana</div>
+              <div class="item" data-value="Adıyaman">Adıyaman</div>
+              <div class="item" data-value="Afyonkarahisar">Afyonkarahisar</div>
+              <div class="item" data-value="Ağrı">Ağrı</div>
+              <div class="item" data-value="Amasya">Amasya</div>
+              <div class="item" data-value="Ankara">Ankara</div>
+              <div class="item" data-value="Antalya">Antalya</div>
+              <div class="item" data-value="Artvin">Artvin</div>
+              <div class="item" data-value="Aydın">Aydın</div>
+              <div class="item" data-value="Balıkesir">Balıkesir</div>
+              <div class="item" data-value="Bilecik">Bilecik</div>
+              <div class="item" data-value="Bingöl">Bingöl</div>
+              <div class="item" data-value="Bitlis">Bitlis</div>
+              <div class="item" data-value="Bolu">Bolu</div>
+              <div class="item" data-value="Burdur">Burdur</div>
+              <div class="item" data-value="Bursa">Bursa</div>
+              <div class="item" data-value="Çanakkale">Çanakkale</div>
+              <div class="item" data-value="Çankırı">Çankırı</div>
+              <div class="item" data-value="Çorum">Çorum</div>
+              <div class="item" data-value="Denizli">Denizli</div>
+              <div class="item" data-value="Diyarbakır">Diyarbakır</div>
+              <div class="item" data-value="Edirne">Edirne</div>
+              <div class="item" data-value="Elazığ">Elazığ</div>
+              <div class="item" data-value="Erzincan">Erzincan</div>
+              <div class="item" data-value="Erzurum">Erzurum</div>
+              <div class="item" data-value="Eskişehir">Eskişehir</div>
+              <div class="item" data-value="Gaziantep">Gaziantep</div>
+              <div class="item" data-value="Giresun">Giresun</div>
+              <div class="item" data-value="Gümüşhane">Gümüşhane</div>
+              <div class="item" data-value="Hakkâri">Hakkâri</div>
+              <div class="item" data-value="Hatay">Hatay</div>
+              <div class="item" data-value="Isparta">Isparta</div>
+              <div class="item" data-value="Mersin">Mersin</div>
+              <div class="item" data-value="İstanbul">İstanbul</div>
+              <div class="item" data-value="İzmir">İzmir</div>
+              <div class="item" data-value="Kars">Kars</div>
+              <div class="item" data-value="Kastamonu">Kastamonu</div>
+              <div class="item" data-value="Kayseri">Kayseri</div>
+              <div class="item" data-value="Kırklareli">Kırklareli</div>
+              <div class="item" data-value="Kırşehir">Kırşehir</div>
+              <div class="item" data-value="Kocaeli">Kocaeli</div>
+              <div class="item" data-value="Konya">Konya</div>
+              <div class="item" data-value="Kütahya">Kütahya</div>
+              <div class="item" data-value="Malatya">Malatya</div>
+              <div class="item" data-value="Manisa">Manisa</div>
+              <div class="item" data-value="Kahramanmaraş">Kahramanmaraş</div>
+              <div class="item" data-value="Mardin">Mardin</div>
+              <div class="item" data-value="Muğla">Muğla</div>
+              <div class="item" data-value="Muş">Muş</div>
+              <div class="item" data-value="Nevşehir">Nevşehir</div>
+              <div class="item" data-value="Niğde">Niğde</div>
+              <div class="item" data-value="Ordu">Ordu</div>
+              <div class="item" data-value="Rize">Rize</div>
+              <div class="item" data-value="Sakarya">Sakarya</div>
+              <div class="item" data-value="Samsun">Samsun</div>
+              <div class="item" data-value="Siirt">Siirt</div>
+              <div class="item" data-value="Sinop">Sinop</div>
+              <div class="item" data-value="Sivas">Sivas</div>
+              <div class="item" data-value="Tekirdağ">Tekirdağ</div>
+              <div class="item" data-value="Tokat">Tokat</div>
+              <div class="item" data-value="Trabzon">Trabzon</div>
+              <div class="item" data-value="Tunceli">Tunceli</div>
+              <div class="item" data-value="Şanlıurfa">Şanlıurfa</div>
+              <div class="item" data-value="Uşak">Uşak</div>
+              <div class="item" data-value="Van">Van</div>
+              <div class="item" data-value="Yozgat">Yozgat</div>
+              <div class="item" data-value="Zonguldak">Zonguldak</div>
+              <div class="item" data-value="Aksaray">Aksaray</div>
+              <div class="item" data-value="Bayburt">Bayburt</div>
+              <div class="item" data-value="Karaman">Karaman</div>
+              <div class="item" data-value="Kırıkkale">Kırıkkale</div>
+              <div class="item" data-value="Batman">Batman</div>
+              <div class="item" data-value="Şırnak">Şırnak</div>
+              <div class="item" data-value="Bartın">Bartın</div>
+              <div class="item" data-value="Ardahan">Ardahan</div>
+              <div class="item" data-value="Iğdır">Iğdır</div>
+              <div class="item" data-value="Yalova">Yalova</div>
+              <div class="item" data-value="Karabük">Karabük</div>
+              <div class="item" data-value="Kilis">Kilis</div>
+              <div class="item" data-value="Osmaniye">Osmaniye</div>
+              <div class="item" data-value="Düzce">Düzce</div>
             </div>
           </div><br>
           <button class="ui animated button right floated <?php echo $color; ?>" tabindex="0">
@@ -53,16 +126,18 @@
     <div class="cities center">
       <?php
         if (isset($_GET['sehirler'])) {
-          $cities = preg_split('/,/', $_GET['sehirler']);
-          $page = 0;
-          if (isset($_GET['sayfa'])) {
-            $page = $_GET['sayfa'];
-          }
-          $first =  5 * $page;
-          $last = 5 * $page + 5;
-          $new_cities = array_slice($cities, $first, $last);
-          foreach ($new_cities as $city) {
-            hava_durumu($city, $color);
+          if ($_GET['sehirler'] != null ) {
+            $cities = preg_split('/,/', $_GET['sehirler']);
+            $page = 0;
+            if (isset($_GET['sayfa'])) {
+              $page = $_GET['sayfa'];
+            }
+            $first =  5 * $page;
+            $last = $first + 5;
+            $new_cities = array_slice($cities, $first, 5);
+            foreach ($new_cities as $city) {
+              hava_durumu($city, $color);
+            }
           }
         }
       ?>
@@ -72,10 +147,11 @@
         <div class="ui buttons right floated">
           <?php
             if ($page != 0) {
-              echo '<a href="?sehirler='.$_GET['sehirler'].'&sayfa='. (--$page) .'" class="ui labeled icon button"><i class="left chevron icon"></i>Geri</a>';
+              echo '<a href="?sehirler='.$_GET['sehirler'].'&sayfa='. ($page - 1) .'" class="ui labeled icon button"><i class="left chevron icon"></i>Geri</a>';
             }
             if (count($cities) > $last) {
-              echo '<a href="?sehirler='.$_GET['sehirler'].'&sayfa='. (++$page) .'" class="ui right labeled icon button">İleri<i class="right chevron icon"></i></a>';
+              echo "page = $page";
+              echo '<a href="?sehirler='.$_GET['sehirler'].'&sayfa='. ($page + 1) .'" class="ui right labeled icon button">İleri<i class="right chevron icon"></i></a>';
             }
           ?>
         </div>
--- sehir.html.php
+++ sehir.html.php
@@ -1,5 +1,6 @@
 <div class="city shadow">
   <?php
+
     echo '<h2 class="text-'.$color.'">'.$data['name'].' '.round($data['main']['temp']).'°</h2>';
     echo '<h3>'.humanize($data['weather'][0]['description']).'</h3><br>';
     echo '<div class="img"><img src="img/'.$data['weather'][0]['icon'].'.png"></div><br>';
